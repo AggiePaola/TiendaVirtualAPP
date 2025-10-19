5c2c86276d8696517a74ea5a1beb2237167871c5
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-10-19 14:52:45 -0500

    splash con animacion

--- app/src/main/java/com/angie/tiendavirtualapp/SplashActivity.kt
+++ app/src/main/java/com/angie/tiendavirtualapp/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.angie.tiendavirtualapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,24 @@ import androidx.core.view.WindowInsetsCompat
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        verBienvenida()
+
+
+    }
+
+    private fun verBienvenida() {
+
+        object : CountDownTimer (3000,1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+            override fun onFinish() {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finishAffinity()
+            }
+        }.start()
+
     }
 }
