10d3d62574320f494d39c21f128f83ba72f8fcc0
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-10-19 15:16:00 -0500

    inicio de sesion vendedor

---
+++ app/src/main/java/com/angie/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
@@ -0,0 +1,18 @@
+package com.angie.tiendavirtualapp.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.angie.tiendavirtualapp.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_vendedor)
+
+        }
+    }
+}
