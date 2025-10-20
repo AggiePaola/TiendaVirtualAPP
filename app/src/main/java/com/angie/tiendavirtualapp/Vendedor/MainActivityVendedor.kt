e60cfebc8ead771a053c97df36e92eedcdfd5add
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-10-19 20:38:07 -0500

    solo front no funcional login y seleccion de usuario

--- app/src/main/java/com/angie/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
+++ app/src/main/java/com/angie/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.angie.tiendavirtualapp.R
+import com.angie.tiendavirtualapp.databinding.ActivityMainVendedorBinding
 
 class MainActivityVendedor : AppCompatActivity() {
+    private lateinit var binding: ActivityMainVendedorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainVendedorBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main_vendedor)
 
         }
