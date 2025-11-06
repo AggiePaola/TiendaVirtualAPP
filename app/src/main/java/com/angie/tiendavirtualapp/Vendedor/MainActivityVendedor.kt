864b0a65e200a80f2c7ebf10e68b26f29e4e1634
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2025-11-05 21:48:52 -0500

    funciones botones

--- app/src/main/java/com/angie/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
+++ app/src/main/java/com/angie/tiendavirtualapp/Vendedor/MainActivityVendedor.kt
@@ -1,20 +1,88 @@
 package com.angie.tiendavirtualapp.Vendedor
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.angie.tiendavirtualapp.R
+import com.angie.tiendavirtualapp.Vendedor.Bottom_Nav_Fragments_Vendedor.FragmentMisProductosV
+import com.angie.tiendavirtualapp.Vendedor.Bottom_Nav_Fragments_Vendedor.FragmentOrdenesV
+import com.angie.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentInicioV
+import com.angie.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentMiTiendaV
+import com.angie.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor.FragmentReseniasV
 import com.angie.tiendavirtualapp.databinding.ActivityMainVendedorBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivityVendedor : AppCompatActivity() {
+class MainActivityVendedor : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
+
     private lateinit var binding: ActivityMainVendedorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainVendedorBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main_vendedor)
+        setContentView(binding.root)
+
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayOut,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        binding.drawerLayOut.addDrawerListener(toggle)
+        toggle.syncState()
+
+        replaceFragment(FragmentInicioV())
+        binding.navigationView.setCheckedItem(R.id.op_inicio_v)
+
+
 
         }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment,fragment)
+            .commit()
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean { //NAVEGAR
+        when (item.itemId) {
+            R.id.op_inicio_v->{
+                replaceFragment(FragmentInicioV())
+            }
+            R.id.op_mi_tienda_v->{
+                replaceFragment(FragmentMiTiendaV())
+            }
+            R.id.op_resenia_v->{
+                replaceFragment(FragmentReseniasV())
+            }
+            R.id.op_cerrar_sesion_v-> {
+                Toast.makeText(applicationContext, "Saliste de la aplicación", Toast.LENGTH_SHORT).show()
+            }
+            R.id.op_mis_productos_v-> {
+                replaceFragment(FragmentMisProductosV())
+            }
+            R.id.op_mis_ordenes_v-> {
+                replaceFragment(FragmentOrdenesV())
+            }
+        }
+        binding.drawerLayOut.closeDrawer(GravityCompat.START)
+            return true
+    }
+}
--- app/src/main/java/com/angie/tiendavirtualapp/Vendedor/Nav_Fragments_Vendedor/FragmentInicioV.kt
+++ app/src/main/java/com/angie/tiendavirtualapp/Vendedor/Nav_Fragments_Vendedor/FragmentInicioV.kt
@@ -1,22 +1,67 @@
 package com.angie.tiendavirtualapp.Vendedor.Nav_Fragments_Vendedor
 
+import android.content.Context
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.angie.tiendavirtualapp.R
+import com.angie.tiendavirtualapp.Vendedor.Bottom_Nav_Fragments_Vendedor.FragmentMisProductosV
+import com.angie.tiendavirtualapp.Vendedor.Bottom_Nav_Fragments_Vendedor.FragmentOrdenesV
+import com.angie.tiendavirtualapp.databinding.FragmentInicioVBinding
 
 
 class FragmentInicioV : Fragment() {
 
+    private lateinit var binding: FragmentInicioVBinding
+    private lateinit var mContext: Context
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_v, container, false)
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
+
+
+    override fun onCreateView(        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        binding = FragmentInicioVBinding.inflate(inflater, container, false)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.op_mis_productos_v->{
+                    replaceFragment(FragmentMisProductosV())
+                }
+                R.id.op_mis_ordenes_v->{
+                    replaceFragment(FragmentOrdenesV())
+                }
+            }
+            true
+        }
+
+        replaceFragment(FragmentMisProductosV())
+        binding.bottomNavigation.selectedItemId = R.id.op_mis_productos_v
+        binding.addFab.setOnClickListener {
+            Toast.makeText(
+                mContext,
+                "Has presionado en el boton flotante",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+
+
+
+
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.bottomFragment,fragment)
+            .commit()
+
     }
 
     }
